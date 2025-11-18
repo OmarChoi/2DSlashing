99a196cae834ac3da708cae785b1fee2a6a92007
Choi JaeJune <omarchoi80@gmail.com>
2025-11-19 03:01:43 +0900

    feat: 표창 충돌 시 플레이어 데미지 적용

---
+++ Assets/02.Scripts/Weapon/WeaponAttack.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class WeaponAttack : MonoBehaviour
+{
+    [SerializeField] private float _damage = 10.0f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") == false) return;
+        other.GetComponent<IDamageable>()?.TakeDamage(_damage);
+        Destroy(gameObject);
+    }
+}
